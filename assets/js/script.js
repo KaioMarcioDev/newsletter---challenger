e3112718479403ce2d7ac71543206e1f71f52876
kaiomarciodev <kaiomarcio.dev@gmail.com>
2023-08-21 15:27:31 -0300

    Function validate()

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,55 @@
 const form = document.querySelector('form')
 const email = document.getElementById('email')
-const modal = document.querySelector('dialog')
+const modal = document.getElementById('janela-modal')
+const alarm = document.querySelector('small')
+const input = document.querySelector('input')
+const span = document.querySelector('span')
+const tex = document.getElementById('tex')
 
 form.addEventListener('submit',(e)=>{
 	e.preventDefault()
-	if(email.value ==''){
-		modal.showModal()
+	if(email.value == ''){
+	    
+		tex.innerHTML = "please fill"
+		input.style.border ="1px solid red"
+
+	}else {
+
+		abrirModal()
+	    fecharJanela()
+
 	}
 
 })
+
+function abrirModal(){
+
+	modal.classList.add('abrir')
+	span.innerHTML = email.value 
+
+}
+
+function fecharJanela(){
+
+	modal.addEventListener('click',(e)=>{
+
+		if(e.target.id == 'fechar' || e.target.id=='janela-modal'){
+			modal.classList.remove('abrir')
+			window.location.href = 'index.html'
+		}
+
+	})
+}
+
+email.addEventListener('keyup',()=>{
+	if(validateEmail(email.value) !== true){
+		alarm.innerHTML = "Format ex:exemple@exemple.com"
+		input.style.border ="1px solid red"
+	}
+})
+
+function validateEmail(email){
+
+	let pattern = /^[_a-z0-9]+(\.[a-z0-9-]+)*@[a-z0-9]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})/
+	return pattern.test(email)
+}
